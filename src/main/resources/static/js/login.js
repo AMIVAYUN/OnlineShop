e2b749e17cae68499aac30a11c5dff79efd55c8f
DESKTOP-45RVDID\leej <zzckckck3@naver.com>
2022-02-12 12:01:02 +0900

    최신화

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,9 +2,5 @@ function login(){
     const form = document.login_form;
     const chkUsername = checkValidUsername(form);
     const chkPw = checkValidPassword(form);
-
-    if(chkUsername){
-        document.getElementById('alert_username').innerText = "";
-
-    }
 }
+

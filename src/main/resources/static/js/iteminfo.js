d1d208a44c1b3bf9af8e06672f691a579a497fcb
DESKTOP-45RVDID\leej <zzckckck3@naver.com>
2022-03-12 12:25:51 +0900

    iteminfo 세부사항 제외 뷰 완료,js추가 예정

---
+++ src/main/resources/static/js/iteminfo.js
@@ -0,0 +1,7 @@
+function show() {//<img>에 이미지 달기
+    document.getElementById("fig").src = "img/static/next.png"
+}
+
+function hide(){ //<img>에 이미지 제거
+    document.getElementById("fig").src="";
+}

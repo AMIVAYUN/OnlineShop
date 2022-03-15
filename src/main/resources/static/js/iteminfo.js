c9fd98ee7c7336ed39d45d434744180c815535e0
DESKTOP-45RVDID\leej <zzckckck3@naver.com>
2022-03-16 01:09:37 +0900

    iteminfo 하단 세부사항 view 수정

--- src/main/resources/static/js/iteminfo.js
+++ src/main/resources/static/js/iteminfo.js
@@ -4,20 +4,32 @@ $(document).ready(function(){
     logoSetting();
     var keyword=KeyWordCheck();
     getItem(keyword);
+    detailBtnSet();
 })
+
+function detailBtnSet(){
+    var btn = document.getElementById("details_explanation_btn");
+    btn.style.backgroundColor = "rgb(225,225,225)"
+}
+
 function show_explanation() {
     var con1 = document.getElementById("details_explanation");
     var con2 = document.getElementById("details_buy");
     var con3 = document.getElementById("details_evaluation");
     var con4 = document.getElementById("details_QnA");
-    var btn = document.getElementById("details_explanation_btn");
+    var btn1 = document.getElementById("details_explanation_btn")
+    var btn2 = document.getElementById("details_buy_btn")
+    var btn3 = document.getElementById("details_evaluation_btn")
+    var btn4 = document.getElementById("details_QnA_btn")
     if(con1.style.display == "none"){
         con1.style.display = "block";
         con2.style.display = "none";
         con3.style.display = "none";
         con4.style.display = "none";
-        btn.style.backgroundColor = "#212425";
-        btn.style.color = "#fff";
+        btn1.style.backgroundColor = "rgb(225,225,225)";
+        btn2.style.backgroundColor = "rgb(245,245,245)";
+        btn3.style.backgroundColor = "rgb(245,245,245)";
+        btn4.style.backgroundColor = "rgb(245,245,245)";
     }
 }
 
@@ -26,11 +38,20 @@ function show_buy() {
     var con2 = document.getElementById("details_buy");
     var con3 = document.getElementById("details_evaluation");
     var con4 = document.getElementById("details_QnA");
+    var btn = document.getElementById("details_buy_btn");
+    var btn1 = document.getElementById("details_explanation_btn")
+    var btn2 = document.getElementById("details_buy_btn")
+    var btn3 = document.getElementById("details_evaluation_btn")
+    var btn4 = document.getElementById("details_QnA_btn")
     if(con2.style.display == "none"){
         con1.style.display = "none";
         con2.style.display = "block";
         con3.style.display = "none";
         con4.style.display = "none";
+        btn1.style.backgroundColor = "rgb(245,245,245)";
+        btn2.style.backgroundColor = "rgb(225,225,225)";
+        btn3.style.backgroundColor = "rgb(245,245,245)";
+        btn4.style.backgroundColor = "rgb(245,245,245)";
     }
 }
 
@@ -39,11 +60,19 @@ function show_evaluation() {
     var con2 = document.getElementById("details_buy");
     var con3 = document.getElementById("details_evaluation");
     var con4 = document.getElementById("details_QnA");
+    var btn1 = document.getElementById("details_explanation_btn")
+    var btn2 = document.getElementById("details_buy_btn")
+    var btn3 = document.getElementById("details_evaluation_btn")
+    var btn4 = document.getElementById("details_QnA_btn")
     if(con3.style.display == "none"){
         con1.style.display = "none";
         con2.style.display = "none";
         con3.style.display = "block";
         con4.style.display = "none";
+        btn1.style.backgroundColor = "rgb(245,245,245)";
+        btn2.style.backgroundColor = "rgb(245,245,245)";
+        btn3.style.backgroundColor = "rgb(225,225,225)";
+        btn4.style.backgroundColor = "rgb(245,245,245)";
     }
 }
 
@@ -52,11 +81,19 @@ function show_QnA() {
     var con2 = document.getElementById("details_buy");
     var con3 = document.getElementById("details_evaluation");
     var con4 = document.getElementById("details_QnA");
+    var btn1 = document.getElementById("details_explanation_btn")
+    var btn2 = document.getElementById("details_buy_btn")
+    var btn3 = document.getElementById("details_evaluation_btn")
+    var btn4 = document.getElementById("details_QnA_btn")
     if(con4.style.display == "none"){
         con1.style.display = "none";
         con2.style.display = "none";
         con3.style.display = "none";
         con4.style.display = "block";
+        btn1.style.backgroundColor = "rgb(245,245,245)";
+        btn2.style.backgroundColor = "rgb(245,245,245)";
+        btn3.style.backgroundColor = "rgb(245,245,245)";
+        btn4.style.backgroundColor = "rgb(225,225,225)";
     }
 }
 async function SessionCheck(){

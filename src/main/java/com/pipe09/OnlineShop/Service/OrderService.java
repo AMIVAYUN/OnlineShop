977b6caabe6b682796b42be3b4d27938b6983736
YUNJUSEOK <wntjrdbs@gmail.com>
2022-04-11 20:15:58 +0900

    1.1_10: 1차 퍼블리싱 준비 완료 ** 0112 @Test annotation mismatch(jupyter.api x) ** ** 0114 BootStrap FrameWork ** ** 0115 login page proto // notice board 완성 ** ** 0116 회원가입 시퀀스 완성 ** ** 0123 메인 홈페이지 디자인 변경 ** ** 0125 메인 홈 get method 완료** ** 0126 관리자 페이지 생성 및 업로드 기능 완료 ** ** 0128 로그인 시스템 및 관리자 권한 완료 ** ** 0128-2 공지사항 등록 및 item 로직 변경(팩토리 메소드 패턴 적용) **
    ** 0129 파일저장 시스템 변경 및 api 리팩토링 + 이미지 파일 외부로 전환(메인 디렉토리 하위 img)+공지사항 조회기능 추가 및 post url 변경**
    
    **0131 FAQ조회 및 삭제 기능 완성
      제품 조회,주문 관리조회,회원 관리 조회,배송 조회 만들어야함. **
    
    ** 0203 jenkins 적용 완료 서버 포트 다시 롤백 -> 8080 **
    
    ** 메인페이지 로그인 세션 적용 **
    
    ** 관리자 페이지 조회기능 전부 적용 **
    해야할 일- 관리자 페이지 꾸미기 및 세부 정보 버튼 설정
    
    ** 0302 관리페이지 수정 기능 완료 **
    ** 검색 시퀀스 완료 **
    
    **0303 공지사항 페이지 완성 **
    **0312 faq
    **Oauth 적용완료 및 각종 페이지 뷰 수정
    ** 메일 서비스 완료 회원만 가능하도록 설정
    
    **0313 check
    
    **0313 iteminfo test
    
    **0313 회사소개 완료
    
    **0318 장바구니 제작중(50%)
     마이페이지 진입 세팅 완료
    **0320 회원가입 리뉴얼 및 MEMBER ENTITY 변경
    
    **0320 장바구니 기능 100%완
    삭제 기능 구현 완료
    
     남은 기능 : 마이페이지 + 결제 기능
    
     **0323 모바일 대비 반응형 가로 세로폭 적용
    
     **0326 결제 페이지 결제 시퀀스 제외 전부 완성(90%)
    
     **0328 Oauth2 에러사항 점검 및 로직 수정완료(모든 서비스 사용 가능) + 보안 강화를 위한 기존 api
       /members/single/local/member_id 를 /members/single/local/aboutMe로 변경 문서 확인 바람.
    ** 0406 결제 서비스 완료
     로고 폰트 수정
     관리자 페이지 수정
    
     **0407 CSRF 보안설정 완료
    
     **0411 Cancel entity 변경(1대다)
     및 결제 취소 기능 구현 완료
    
     **0411 1차 퍼블리싱.

--- src/main/java/com/pipe09/OnlineShop/Service/OrderService.java
+++ src/main/java/com/pipe09/OnlineShop/Service/OrderService.java
@@ -161,6 +161,7 @@ public class OrderService {
     @Transactional
     public boolean SuccessHandle(Long Order_id,String paymentKey,int amount){
         Orders order=orderRepository.findOne(Order_id);
+        log.info(order.getOrder_ID()+"객체 확인");
         order.setDeliverystatus(Deliverystatus.READY);
         order.setPaymentKey(paymentKey);
 
